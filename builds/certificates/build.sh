2fbe9881c2726495c9aacaea28b29fdfef946873
Blake Glanville <blake@digivigil.com>
2022-07-09 10:25:11 -0700

    Copy build directory from established Coopilot

---
+++ builds/certificates/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#this is meant to be executed from the root repo dir using npm
+echo "I'm in $(pwd)"
+openssl genrsa -out src/balek-server/etc/cert/auto-ca.key 4096
+openssl req -x509 -new -nodes -key src/balek-server/etc/cert/auto-ca.key -sha256 -days 365 -out src/balek-server/etc/cert/auto-ca.crt -subj "/C=US/ST=Oregon/L=Oregon/O=Private/OU=auto/CN=coopilot"
+openssl genrsa -out src/balek-server/etc/cert/auto-server.key 4096
+openssl req -new -key src/balek-server/etc/cert/auto-server.key -config builds/certificates/config.cnf -out src/balek-server/etc/cert/auto.csr
+
