abcd5c29e74b2e419866258b23e43e8ae8dccf70
coop-blake <96803255+coop-blake@users.noreply.github.com>
2022-07-09 12:42:07 -0700

    Update build.sh

--- builds/certificates/build.sh
+++ builds/certificates/build.sh
@@ -6,4 +6,4 @@ mkdir autogenCerts
 openssl genrsa -out autogenCerts/auto-ca.key 4096
 openssl req -x509 -new -nodes -key autogenCerts/auto-ca.key -sha256 -days 365 -out autogenCerts/auto-ca.crt -subj "/C=US/ST=Oregon/L=Oregon/O=Private/OU=auto/CN=coopilot"
 openssl genrsa -out autogenCerts/auto-server.key 4096
-#openssl req -new -key autogenCerts/auto-server.key -config builds/certificates/config.cnf -out autogenCerts/auto.csr
+openssl req -new -key autogenCerts/auto-server.key -config autogenCertConfig.cnf -out autogenCerts/auto.csr
