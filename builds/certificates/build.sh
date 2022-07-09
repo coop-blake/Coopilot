f40156dd74315814da9333fa018dc55f2db7c23c
Blake Glanville <blake@digivigil.com>
2022-07-09 11:48:43 -0700

    Moved Autogen Certificates
    Added check in configManager to use autogenerated certificates if user certificates are not found

--- builds/certificates/build.sh
+++ builds/certificates/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #this is meant to be executed from the root repo dir using npm
 echo "I'm in $(pwd)"
-echo "Building Self Signed Certificates in src/balek-server/etc/cert"
-mkdir -p src/balek-server/etc/cert
-openssl genrsa -out src/balek-server/etc/cert/auto-ca.key 4096
-openssl req -x509 -new -nodes -key src/balek-server/etc/cert/auto-ca.key -sha256 -days 365 -out src/balek-server/etc/cert/auto-ca.crt -subj "/C=US/ST=Oregon/L=Oregon/O=Private/OU=auto/CN=coopilot"
-openssl genrsa -out src/balek-server/etc/cert/auto-server.key 4096
-openssl req -new -key src/balek-server/etc/cert/auto-server.key -config builds/certificates/config.cnf -out src/balek-server/etc/cert/auto.csr
+echo "Building Self Signed Certificates in $(pwd)/autogenCerts"
+mkdir autogenCerts
+openssl genrsa -out autogenCerts/auto-ca.key 4096
+openssl req -x509 -new -nodes -key autogenCerts/auto-ca.key -sha256 -days 365 -out autogenCerts/auto-ca.crt -subj "/C=US/ST=Oregon/L=Oregon/O=Private/OU=auto/CN=coopilot"
+openssl genrsa -out autogenCerts/auto-server.key 4096
+openssl req -new -key autogenCerts/auto-server.key -config builds/certificates/config.cnf -out autogenCerts/auto.csr
